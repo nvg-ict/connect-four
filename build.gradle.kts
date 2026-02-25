af715414b96869b86d5b677ea172899a84868b0f
Nico van Gerwen <nico.vangerwen@ns.nl>
2026-02-25 11:26:29 +0100

    feat: Initial setup of kotlin project

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    kotlin("jvm") version "2.3.0"
+}
+
+group = "nl.craftsmen"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+kotlin {
+    jvmToolchain(17)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
