cbe51341139134a557b9e1cfc0860e05d13da0a5
Nico van Gerwen <nico.vangerwen@ns.nl>
2026-03-08 19:21:32 +0100

    feat: Player {int} has just completed 4-in-a-row

--- src/test/kotlin/connect/four/cucumber/S10-GameEnd.kt
+++ src/test/kotlin/connect/four/cucumber/S10-GameEnd.kt
@@ -1,29 +1,31 @@
-@file:Suppress("UnusedParameter")
-
 package connect.four.cucumber
 
+import connect.four.Game
+import connect.four.GameRules
 import io.cucumber.java.en.Given
 import io.cucumber.java.en.Then
 import io.cucumber.java.en.When
 
 class GameEnd {
+    private lateinit var game: Game
+
     @Given("Player {int} has just completed 4-in-a-row")
     fun playerHasJustCompletedInARow(player: Int) {
-        TODO("Implement step")
-    }
+        game = Game(GameRules())
 
-    @Given("the board is completely full with no 4-in-a-row for either player")
-    fun boardIsCompletelyFullWithNoInARow() {
-        TODO("Implement step")
+        val moves = when (player) {
+            1 -> listOf(1, 6, 2, 6, 3, 6, 4)
+            2 -> listOf(6, 1, 6, 2, 6, 3, 5, 4)
+            else -> error("Unsupported player: $player")
+        }
+
+        moves.forEach { column ->
+            game.applyMove(column)
+        }
     }
 
     @When("the game ends")
     fun theGameEnds() {
-        TODO("Implement step")
-    }
-
-    @Then("the final board is displayed")
-    fun theFinalBoardIsDisplayed() {
         TODO("Implement step")
     }
 
