71155ab00ae620196c37626157ecd8b9c8ef0af2
Nico van Gerwen <nico.vangerwen@ns.nl>
2026-03-08 09:17:12 +0100

    feat: Introduce story 8

---
+++ src/test/kotlin/connect/four/cucumber/S08-VerticalDiagonalWin.kt
@@ -0,0 +1,50 @@
+package connect.four.cucumber
+
+import io.cucumber.datatable.DataTable
+import io.cucumber.java.en.And
+import io.cucumber.java.en.Given
+import io.cucumber.java.en.Then
+import io.cucumber.java.en.When
+
+class WinDetectionSteps {
+
+    @Given("column {int} has {int} red coins \\(🔴\\) stacked consecutively from row {int} to row {int}")
+    fun columnHasRedCoinsStacked(column: Int, coins: Int, rowFrom: Int, rowTo: Int) {
+        TODO("Implement step")
+    }
+
+    @When("Player {int} drops a final coin in column {int}")
+    fun playerDropsFinalCoinInColumn(player: Int, column: Int) {
+        TODO("Implement step")
+    }
+
+    @Then("the game detects a vertical win for Player {int}")
+    fun theGameDetectsVerticalWinForPlayer(player: Int) {
+        TODO("Implement step")
+    }
+
+    @And("the game ends immediately")
+    fun theGameEndsImmediately() {
+        TODO("Implement step")
+    }
+
+    @Given("the board has {int} yellow coins \\(🟡\\) on an upward-right diagonal")
+    fun boardHasYellowCoinsOnDiagonal(coins: Int) {
+        TODO("Implement step")
+    }
+
+    @And("the coordinates are:")
+    fun theCoordinatesAre(table: DataTable) {
+        TODO("Implement step")
+    }
+
+    @When("Player {int} drops a final coin completing the diagonal")
+    fun playerDropsFinalCoinCompletingDiagonal(player: Int) {
+        TODO("Implement step")
+    }
+
+    @Then("the game detects a diagonal win for Player {int}")
+    fun theGameDetectsDiagonalWinForPlayer(player: Int) {
+        TODO("Implement step")
+    }
+}
---
+++ src/test/resources/features/S08-VertficalDiagonalWin.feature
@@ -0,0 +1,19 @@
+Feature: Vertical and Diagonal Win
+
+  Scenario: Player 2 wins with 4 consecutive coins stacked vertically
+    Given column 4 has 4 red coins (🔴) stacked consecutively from row 1 to row 4
+    When Player 2 drops a final coin in column 4
+    Then the game detects a vertical win for Player 2
+    And the game ends immediately
+
+  Scenario: Player 1 wins with 4 coins in upward-right diagonal (↗)
+    Given the board has 4 yellow coins (🟡) on an upward-right diagonal
+    And the coordinates are:
+      | row | col |
+      | 1   | 2   |
+      | 2   | 3   |
+      | 3   | 4   |
+      | 4   | 5   |
+    When Player 1 drops a final coin completing the diagonal
+    Then the game detects a diagonal win for Player 1
+    And the game ends immediately
